f0b5f808e58c7db49d89f58e20d3c6af5dc4c64e
John Eblahan <johneblahan@gmail.com>
2021-11-10 20:15:26 -0500

    Wall Paintings + Cloud Movement + Player Direction + Anims

--- DownHorrendous_Unity/Assets/Wesley Folder/Scripts/LightSwitch.cs
+++ DownHorrendous_Unity/Assets/Wesley Folder/Scripts/LightSwitch.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public class LightSwitch : MonoBehaviour
 {
     public bool isOn;
+    
+
+    private void Update()
+    {
+        if (isOn)
+        {
+            gameObject.GetComponent<Animator>().SetBool("isPressed", true);
+        }
+        else {
+            gameObject.GetComponent<Animator>().SetBool("isPressed", false);
+        }
+    }
 }
